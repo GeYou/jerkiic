af9f114c44298f1defaf0a6d484d158798b9eb86
Ivan Wesley Chua <ivanwesleychua@yahoo.com>
2015-07-29 22:32:08 +0800

    new test

---
+++ beta tests/ThesisBeta/app/src/main/java/com/thesis/thesisbeta/MainActivity.java
@@ -0,0 +1,40 @@
+package com.thesis.thesisbeta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends Activity {
+
+    static final LatLng IvanPos = new LatLng(40, -79);
+    private GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            if(googleMap == null) {
+                googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            }
+
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            googleMap.setMyLocationEnabled(true);
+            googleMap.setTrafficEnabled(true);
+            googleMap.setIndoorEnabled(true);
+            googleMap.setBuildingsEnabled(true);
+            googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+            Marker marker = googleMap.addMarker(new MarkerOptions().position(IvanPos).title("Hello"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

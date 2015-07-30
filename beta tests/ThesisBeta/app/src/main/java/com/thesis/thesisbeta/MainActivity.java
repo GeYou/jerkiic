3f398d57fbc5c800930b977a0a16988568772dec
Ivan Wesley Chua <ivanwesleychua@yahoo.com>
2015-07-30 10:49:46 +0800

    working

--- beta tests/ThesisBeta/app/src/main/java/com/thesis/thesisbeta/MainActivity.java
+++ beta tests/ThesisBeta/app/src/main/java/com/thesis/thesisbeta/MainActivity.java
@@ -2,7 +2,10 @@ package com.thesis.thesisbeta;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,12 +22,53 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            if(googleMap == null) {
-                googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-            }
+        createMap();
+        addMarker();
+    }
 
-            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    private void createMap() {
+        try {
+            if(null == googleMap){
+                googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                        R.id.map)).getMap();
+                if(null == googleMap) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error creating map", Toast.LENGTH_SHORT).show();
+                }
+                /*else {
+                    googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    googleMap.setMyLocationEnabled(true);
+                    googleMap.setTrafficEnabled(true);
+                    googleMap.setIndoorEnabled(true);
+                    googleMap.setBuildingsEnabled(true);
+                    googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+                    Marker marker = googleMap.addMarker(new MarkerOptions().position(IvanPos).title("Hello"));
+
+
+                    LatLng latlng = new LatLng(10.351993898, 123.913227877);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+
+            // Zoom in the Google Map
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(8));
+            googleMap.addMarker(new MarkerOptions()
+                            .position(latlng)
+                            .title("Marker")
+                            .draggable(true)
+            );
+                }*/
+            }
+        } catch (NullPointerException exception){
+            Log.e("mapApp", exception.toString());
+        }
+    }
+    private void addMarker() {
+
+        /** Make sure that the map has been initialised **/
+        if (null != googleMap) {
+
+
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             googleMap.setMyLocationEnabled(true);
             googleMap.setTrafficEnabled(true);
             googleMap.setIndoorEnabled(true);
@@ -32,9 +76,7 @@ public class MainActivity extends Activity {
             googleMap.getUiSettings().setZoomControlsEnabled(true);
 
             Marker marker = googleMap.addMarker(new MarkerOptions().position(IvanPos).title("Hello"));
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
+
 }
